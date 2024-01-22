60568495e487df55745b255208e00bdedb839e25
abhinav0306 <abhinavs0603@gmail.com>
2024-01-22 16:29:26 +0530

    files added

---
+++ custom/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import useStorage from './useStorage'
+
+const Home = () => {
+  return (
+    <div>hi</div>
+  )
+}
+
+export default Home
---
+++ custom/src/components/UseStorage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+
+const useStorage = () => {
+  return (
+    <div>useStorage</div>
+  )
+}
+
+export default useStorage
