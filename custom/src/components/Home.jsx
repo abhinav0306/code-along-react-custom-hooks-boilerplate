730f07470bf09e08165d76eb8104b331f34a36ac
abhinav0306 <abhinavs0603@gmail.com>
2024-01-22 17:00:07 +0530

    done

--- custom/src/components/Home.jsx
+++ custom/src/components/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import useStorage from './useStorage'
-
+import UseStorage from './useStorage'
+import "./home.css"
 const Home = () => {
+    const [inputValue, setInputValue] = UseStorage("inputValue");
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setInputValue(value);
+  };
   return (
-    <div>hi</div>
-  )
+<div className="box">
+<input
+        type="text"
+        value={inputValue}
+        onChange={handleChange}
+      />
+      </div>
+    )
 }
 
 export default Home
